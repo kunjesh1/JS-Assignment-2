b7448abce0f1564860645665a6a66409be278ab8
kunjesh1 <kumarkunjesh1@gmail.com>
2019-06-21 18:49:57 +0530

    added pre-formatted tag
    added pre-formatted tag

--- main.js
+++ main.js
@@ -19,10 +19,7 @@ var result =
 
 function myFunction() {
     var search = document.getElementById("myText").value;
-    document.getElementById("demo").innerHTML = search;
-    //'https://api.github.com/search/repositories?q={{'+search+'}}'
-    // var ourRequest = new XMLHttpRequest();
-    // ourRequest.open('GET','fdfjfdj' );
+    
 
 
 
@@ -67,11 +64,10 @@ function myFunction() {
                 });
                 
 
-                var combinedData = { "apiRequest1": {}, "apiRequest2": {} };
-                Promise.all([apiRequest1, apiRequest2]).then(function (values) {
-                    combinedData["apiRequest1"] = values[0];
-                    combinedData["apiRequest2"] = values[1];
-                    return combinedData;
+               
+                Promise.all([apiRequest1, apiRequest2]).then(function () {
+                      document.getElementById("output").innerHTML=JSON.stringify(result,undefined,2);
+                   
                 }).catch(err=>console.log(err));
 
             }
